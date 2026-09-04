4391d0b2530d68473ee974532db5feda51f4a961
Ulises Gamboa <33880229+ulisesgamboa@users.noreply.github.com>
2026-09-04 11:39:21 -0600

    Delete report_isn directory

--- report_isn/report_isn/models/hr_salary_rule.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-
-from odoo import api, fields, models, _
-
-class HrSalaryRule(models.Model):
-    _inherit = 'hr.salary.rule'
-
-    integrar_isn = fields.Boolean('Incluir en ISN')
